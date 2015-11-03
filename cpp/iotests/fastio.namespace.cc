79a96ba45364af9c4685b60053ffaa7fd3d0e368
eightnoteight <mr.eightnoteight@gmail.com>
2015-11-03 18:35:07 +0530

    converted fastio as struct to support the operators, and only fast input has completed.

--- cpp/iotests/fastio.namespace.cc
+++ cpp/iotests/fastio.namespace.cc
@@ -8,7 +8,7 @@
 using namespace std;
 const ulong mod = 1000000007ul;
 
-namespace fastio {
+struct Fastio {
     inline char ignorewhits() {
         char c;
         do {
@@ -16,9 +16,11 @@ namespace fastio {
         } while(c != -1 && c < 33);
         return c;
     }
+
     inline void scan(char& x) {
         x = ignorewhits();
     }
+
     inline void scan(char* w) {
         *w = ignorewhits();
         if (*w == -1) {
@@ -30,6 +32,7 @@ namespace fastio {
                 break;
         *w = 0;
     }
+
     inline void scan(string& w) {
         w.clear();
         w.push_back(ignorewhits());
@@ -45,36 +48,70 @@ namespace fastio {
         }
         w.pop_back();
     }
-    template<class intType>
-    inline void scan(intType& p) {
-        char c;
-        bool sign = false;
-        p = 0;
-        c = ignorewhits();
-        if (c == '-') {
-            sign = true;
-            c = getchar_unlocked();
-        }
-        while (c > 33) {
-            p = (p * 10) + (c - '0');
-            c = getchar_unlocked();
-        }
-        if (sign)
-            p *= -1;
+
+    #define SCANIREF_FOR(intType) \
+    inline void scan(intType& p) { \
+        char c; \
+        bool sign = false; \
+        p = 0; \
+        c = ignorewhits(); \
+        if (c == '-') { \
+            sign = true; \
+            c = getchar_unlocked(); \
+        } \
+        while (c > 33) { \
+            p = (p * 10) + (c - '0'); \
+            c = getchar_unlocked(); \
+        } \
+        if (sign) \
+            p *= -1; \
     }
+    SCANIREF_FOR(int)
+    SCANIREF_FOR(uint)
+    SCANIREF_FOR(long)
+    SCANIREF_FOR(ulong)
+
     template<class dType, class... Args>
     inline void scan(dType& dt, Args&... args) {
         scan(dt);
         scan(args...);
     }
-}
+    template<class T>
+    inline Fastio& operator >> (T& item) {
+        scan(item);
+        return *this;
+    }
+    template<class T>
+    inline Fastio& operator << (T& item) {
+        print(item);
+        return *this;
+    }
+    inline void print(char x) {
+
+    }
+};
+Fastio fastio;
 
 int main(int argc, char const *argv[]) {
-    ulong* arr = new ulong[10000001];
-    uint k = 0;
-    for_each(arr, arr + 10000001, [&k](ulong& x){
-        fastio::scan(x);
-        k += __builtin_popcountll(x);
-    });
+    int varint1;
+    int varint2;
+    long varlong1;
+    long varlong2;
+    ulong varulong1;
+    ulong varulong2;
+    char str1[100];
+    string str2;
+    char varch;
+    fastio >> \
+        varint1 >> \
+        varint2 >> \
+        varlong1 >> \
+        varlong2 >> \
+        varulong1 >> \
+        varulong2 >> \
+        str1 >> \
+        str2 >> \
+        varch;
+    cout << varint1 << ' ' << varint2 << ' ' << varlong1 << ' ' << varlong2 << ' ' << varulong1 << ' ' << varulong2 << ' ' << str1 << ' ' <<  str2 << ' ' << varch << '\n';
     return 0;
 }
