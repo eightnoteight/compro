e5f9c5491e8d5eb5f432a14dce9d6cabbeff062b
eightnoteight <mr.eightnoteight@gmail.com>
2015-12-18 16:41:07 +0530

    whatever i couldn't solve seadiv

--- codechef/dec15/seadiv.py
+++ codechef/dec15/seadiv.py
@@ -3,11 +3,6 @@ from sys import stdin
 def main():
     nextitem = iter(stdin.read().split()).next
     for _ in xrange(int(nextitem())):
-        c10 = (int(nextitem(), 7) // int(nextitem(), 7)) % pow(7, int(nextitem()))
-        arr = bytearray()
-        while c10:
-            c10, c = divmod(c10, 7)
-            arr.append(str(c))
-        print arr[::-1].lstrip('0') or '0'
+        print int(str(int(nextitem(), 7) // int(nextitem(), 7))[-int(nextitem()):])
 
 main()
