d5cfff9ffd303cbd62f6973abbd06b529237890e
eightnoteight <mr.eightnoteight@gmail.com>
2016-01-04 07:57:51 +0530

    basic nim game

---
+++ leetcode/nim_game.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+
+
+class Solution(object):
+    def canWinNim(self, n):
+        return n % 4 != 0
+
+
+def main():
+    sol = Solution()
+    for _ in xrange(int(raw_input())):
+        print sol.canWinNim(int(raw_input()))
+
+if __name__ == '__main__':
+    main()
