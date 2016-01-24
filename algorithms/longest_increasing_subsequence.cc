ce4de9e43b0a3a85714173545488e92a1f432695
eightnoteight <mr.eightnoteight@gmail.com>
2016-01-24 18:01:18 +0530

    longest increasing subsequence and length of the longest increasing subsequence.

--- algorithms/longest_increasing_subsequence.cc
+++ algorithms/longest_increasing_subsequence.cc
@@ -26,7 +26,31 @@ uint bsearch(uint lo, uint hi, Function check) {
     return lo;
 }
 
-uint longest_increasing_subsequece(vector<uint> arr) {
+vector<uint> get_longest_increasing_subsequece(vector<uint> arr) {
+    vector<uint> lis(len(arr), t_max(uint));
+    vector<uint> lis_arr(len(arr));
+    lis[0] = arr[0];
+    lis_arr[0] = 1;
+    uint lis_length = 1;
+    for (uint i = 1; i < len(arr); ++i) {
+        uint k = bsearch(0, len(lis), [&](uint x) -> bool {
+            return lis[x] >= arr[i];
+        });
+        if (arr[i] < lis[k])
+            lis[k] = arr[i];
+        if (k >= lis_length)
+            lis_length = k + 1;
+        lis_arr[i] = k + 1;
+    }
+    vector<uint> subseq(lis_length);
+    for (uint i = 0; i < len(arr); ++i) {
+        if (subseq[lis_arr[i]] < arr[i])
+            subseq[lis_arr[i]] = arr[i];
+    }
+    return subseq;
+}
+
+uint get_longest_increasing_subsequece_len(vector<uint> arr) {
     vector<uint> lis(len(arr), t_max(uint));
     lis[0] = arr[0];
     uint lis_length = 1;
@@ -46,9 +70,13 @@ int main(int argc, char const *argv[]) {
     #ifndef __mr__
         ios::sync_with_stdio(0);cin.tie(0);
     #endif
-    cout << longest_increasing_subsequece(
+    cout << get_longest_increasing_subsequece(
         vector<uint>{10, 22, 9, 33, 21, 50, 41, 60, 80}) << endl;
-    cout << longest_increasing_subsequece(
+    cout << get_longest_increasing_subsequece(
+        vector<uint>{1, 4, 2, 4, 3}) << endl;
+    cout << get_longest_increasing_subsequece_len(
+        vector<uint>{10, 22, 9, 33, 21, 50, 41, 60, 80}) << endl;
+    cout << get_longest_increasing_subsequece_len(
         vector<uint>{1, 4, 2, 4, 3}) << endl;
     return 0;
 }
